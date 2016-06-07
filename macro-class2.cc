309917aa69f2b24bef88a6267e2e9639aefdd421
rainySue <410692979@qq.com>
2016-06-07 20:11:06 +0800

    try register

--- macro-class2.cc
+++ macro-class2.cc
@@ -6,6 +6,45 @@ using namespace std;
 
 typedef void* (*createClass)(void) ; 
 
+class CKClassFactory  
+{  
+public:  
+    CKClassFactory(){}
+   
+    virtual ~CKClassFactory(){}
+   
+    void* getClassByName(string className) ;  
+   
+    void registClass(string name, createClass method) ;  
+   
+    static CKClassFactory& sharedClassFactory() ;  
+   
+private:  
+    map<string, createClass> m_classMap ;  
+} ;  
+
+void* CKClassFactory::getClassByName(string className)  
+{  
+    map<string, createClass>::const_iterator iter ;  
+   
+    iter = m_classMap.find(className) ;  
+    if ( iter == m_classMap.end() )  
+        return NULL ;  
+    else  
+        return iter->second() ;  
+}  
+   
+void CKClassFactory::registClass(string name, createClass method)  
+{  
+    m_classMap.insert(pair<string, createClass>(name, method)) ;  
+}  
+   
+CKClassFactory& CKClassFactory::sharedClassFactory()  
+{  
+    static CKClassFactory _sharedClassFactory ;  
+    return _sharedClassFactory ;  
+}  
+
 class CKBaseClass  
 {  
 public:  
@@ -35,11 +74,30 @@ public:
   name ## Class::name ## Class ()                                       \
     : CKBaseClass("default") {}                                         \
   name ## Class::name ## Class (const string& str)                      \
-    : CKBaseClass("str") {}                                                \
-  void name ## Class::display () {                             \
-    cout << #name << "Class" <<  endl;                                               \
+    : CKBaseClass("str") {}                                             \
+  void name ## Class::display () {                                      \
+    cout << #name << "Class" <<  endl;                                  \
     CKBaseClass::display();                                             \
   }        
+
+  class ExampleClass : public  CKBaseClass
+  {  
+  private:  
+      DECLARE_CLASS(ExampleClass)  
+     
+  public:  
+      ExampleClass() {}  
+      virtual ~ExampleClass() {}  
+      static void* createInstance();
+      virtual void registProperty() {}  
+      virtual void display() {
+        cout << "ExampleClass" << endl;
+      }  
+  } ;  
+
+  void* ExampleClass::createInstance()
+   {return new ExampleClass();}  
+
 
   DECLARE_CLASS(first)
   IMPLEMENT_CLASS(first)
@@ -47,8 +105,11 @@ public:
   int main()
   {
     CKBaseClass* cla = new firstClass;
-    // // CKBaseClass* cla = (CKBaseClass*)(firstClass::createInstance());
-
+    // // 
+    CKClassFactory::sharedClassFactory().registClass("ExampleClass", ExampleClass::createInstance);
+    CKBaseClass* cla1 = (CKBaseClass*)CKClassFactory::sharedClassFactory().getClassByName("ExampleClass");
+ 
     cla->display();
+    cla1->display();
     return 0;
   }                                                              
